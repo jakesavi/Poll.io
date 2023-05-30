ae497b3002d56f5b0ffc5c3c01dea67651fa41bc
jakesavi <jakesavi@buffalo.edu>
2023-05-30 06:11:17 -0700

    Removed poll method for right now. It doesn't work :(

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
 import discord
-from babble import babble, coinflip
+from babble import *
 from secret import *
 import pollEvents
 
@@ -12,15 +11,16 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
-
+    
 @client.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == client.user: # Ignore self function
         return
 
-    
+
     # Each of these sections are different "Functions" to pollio message commands.
     # I am unsure if there is a better way to do this
+    
     # Random word search
     if message.content.startswith('$babble'):
         listOfBabble = babble()
@@ -38,30 +38,17 @@ async def on_message(message):
             await message.channel.send("You landed Tails!")
             return
 
+# TODO FIX the parsing with $roll
+    if message.content.startswith("$roll "):
+        if(len(message.content)) > 5 and 'd' in message.content:
+            rolls = roll(int(message.content[6]),int(message.content[8]))
+            for j in rolls:
+                await message.channel.send(j)
+            await message.channel.send("Your total is: " + str(sum(rolls)))
 
-    # Create a poll!
-    if message.content.startswith("$createPoll"):
-        if not message.channel.permissions_for(message.author).kick_members: 
-            
-            await message.channel.send("You are not a certified user")
+
         else:
-            await message.channel.send("Ok! What would you like the name of the poll to be?")
-            
-            # check function that is optional with wait_for() see wait_for() in pycord documentation
-            DiscussedChannel = message.channel
-            # Ensures that the message matches the user (PLEASE TAKE NOTE OF SCOPE OF THIS FUNCTION)
+            await message.channel.send("Incorrect Syntax")
 
-
-            def match_certified_user(m):
-                return m.author == message.author
-
-            pollTitle = await client.wait_for('message',check=match_certified_user, timeout = 180.0)
-            await DiscussedChannel.send("Alright, creating poll: " + pollTitle.content)
-            await message.channel.send("Ok now tell me the options seperated by ',' If there are no comma's there will only be one option! ALSO DON'T PUT A SPACE BETWEEN OPTIONS PLEASE!")
-            pollOptions = await client.wait_for('message',check=match_certified_user, timeout = 200.0)
-            pollOptionsArray = pollOptions.content.split(',')
-            await message.channel.send(pollEvents.makePoll(pollTitle, pollOptionsArray)["url"])
-        
-
-  #This should be the last thing executed as this launches the bot.  
+#This should be the last thing executed as this launches the bot.
 client.run(token())
